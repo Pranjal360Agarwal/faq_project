0c81b4e9be4649165789024e10cd5abcb0ebba7e
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2025-02-03 11:50:46 +0530

    feat: implement REST API for FAQs with language selection

--- faqs/views.py
+++ faqs/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status, generics
+from django.shortcuts import get_object_or_404
+from .models import FAQ
+from .serializers import FAQSerializer
 
-# Create your views here.
+
+class FAQListCreateView(generics.ListCreateAPIView):
+    queryset = FAQ.objects.all()
+    serializer_class = FAQSerializer
+
+    def get(self, request, *args, **kwargs):
+        lang = request.GET.get("lang", "en")
+        faqs = FAQ.objects.filter(language="en")
+        data = [faq.get_translation(lang) for faq in faqs]
+        return Response(data, status=status.HTTP_200_OK)
